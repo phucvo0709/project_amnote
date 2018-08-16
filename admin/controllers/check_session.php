60ce15b1c3b2e8b21cd6e457c21abdf6d7c12064
Phuc Vo <phucvo0709@gmail.com>
2018-08-16 12:15:26 +0700

    add check_session

--- admin/controllers/check_session.php
+++ admin/controllers/check_session.php
@@ -5,8 +5,11 @@ if (isset($_SESSION['id']) == false) {
     header('Location: login');
     exit();
 }else{
-    if(isset($_SESSION['permission']) == 0){
-        header('Location: logout');
-        exit();
+    if($_SESSION['permission'] == 0){
+        echo
+        "<script>
+                window.location.href='logout';
+                alert('Your account is not authorized to enter the admin panel');
+		</script>";
     }
 }
